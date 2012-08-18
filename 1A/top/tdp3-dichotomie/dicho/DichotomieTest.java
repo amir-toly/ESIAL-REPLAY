39e99100fe56c93cdd068806c90f6f28a1de72f9
Amir TOLY <amir.toly@gmail.com>
2012-08-19 00:24:40 +0200

    FAIL: Infinite recursion

--- 1A/top/tdp3-dichotomie/dicho/DichotomieTest.java
+++ 1A/top/tdp3-dichotomie/dicho/DichotomieTest.java
@@ -3,16 +3,13 @@ package dicho;
 public class DichotomieTest {
 	
 	public static void main(String[] args) {
-		int[] tab = new int[100];
+		int[] tab = new int[]{1, 5, 6, 100};
 		Dichotomie dicho = new Dichotomie();
 		
-		for (int i=0;i<tab.length;i++) {
-			tab[i] = i + 1;
-		}
 		System.out.println("Expected: 64");
 		System.out.println("Looking for the index " +
 				"corresponding to the value 65...");
-		System.out.println(dicho.find(65, tab, 1, 100));
+		System.out.println(dicho.find(5, tab, 2, 4));
 	}
 }
 
