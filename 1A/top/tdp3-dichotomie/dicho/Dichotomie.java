2a9fd4d2078cf3f989f03bbca0d601f7e64e4e01
Amir TOLY <amir.toly@gmail.com>
2012-08-18 13:57:02 +0200

    Add javadoc

--- 1A/top/tdp3-dichotomie/dicho/Dichotomie.java
+++ 1A/top/tdp3-dichotomie/dicho/Dichotomie.java
@@ -2,6 +2,22 @@ package dicho;
 
 public class Dichotomie {
 	
+	/**
+	 * Search the specified element in the tab array between borneInf & borneSup
+	 * indexes, starting at 1.
+	 * <p>
+	 * Preconditions:
+	 * - the tab array must be sorted in ascending order;
+	 * - the borneInf parameter must be greater or equal to 1;
+	 * - the borneInf parameter must be lower than the borneSup parameter;
+	 * - the borneSup parameter must be lower or equal to tab.length.
+	 * 
+	 * @param	element		the element we are looking for
+	 * @param	tab			the array we are looking in
+	 * @param	borneInf	the lower bound (at least 1)
+	 * @param	borneSup	the upper bound (at most tab.length)
+	 * @return				the element index if found, -1 otherwise
+	 */
 	public int find(int element, int[] tab, int borneInf, int borneSup) {
 		// Retrieve actual indexes
 		borneInf--;
