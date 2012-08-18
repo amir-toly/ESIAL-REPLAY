4a8a13cd6f54c21fd64e8e09dadd515e1c21e5d2
Amir TOLY <amir.toly@gmail.com>
2012-08-18 12:54:08 +0200

    Now the search will be performed from (at least) 0 to (at most) the length of the array

--- 1A/top/tdp3-dichotomie/dicho/Dichotomie.java
+++ 1A/top/tdp3-dichotomie/dicho/Dichotomie.java
@@ -3,6 +3,10 @@ package dicho;
 public class Dichotomie {
 	
 	public int find(int element, int[] tab, int borneInf, int borneSup) {
+		// Retrieve actual indexes
+		borneInf--;
+		borneSup--;
+		
 		if (borneInf == borneSup) {
 			return -1;
 		} else {
@@ -10,9 +14,10 @@ public class Dichotomie {
 			int odd = median % 2 == 0 ? 0 : 1;
 			median = (median + odd) / 2;
 			switch (Integer.valueOf(element).compareTo(tab[median])) {
-				case 0: return median;
-				case -1: return find(element, tab, borneInf, median);
-				default: return find(element, tab, median, borneSup);
+				// From the outside, indexes start at 1 (instead of 0), hence the +1
+				case 0: return median + 1;
+				case -1: return find(element, tab, borneInf + 1, median + 1);
+				default: return find(element, tab, median + 1, borneSup + 1);
 			}
 		}
 	}
